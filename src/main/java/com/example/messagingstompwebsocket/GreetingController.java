443f56f15810484b204d02d41e36c16317b48187
yj-melissa <melimelame@gmail.com>
2024-04-21 16:52:07 +0900

    GreetingController의 불필요한 import문 삭제

--- src/main/java/com/example/messagingstompwebsocket/GreetingController.java
+++ src/main/java/com/example/messagingstompwebsocket/GreetingController.java
@@ -2,7 +2,6 @@ package com.example.messagingstompwebsocket;
 
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.messaging.handler.annotation.SendTo;
-import org.springframework.messaging.simp.annotation.SendToUser;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.util.HtmlUtils;
 
